ae05905878b9806ebc5198efef55ec647d248104
Nikola Kolev <nickkolev06@abv.bg>
2023-02-13 21:43:19 +0200

    longest palindrome

---
+++ src/TopInterviewQuestions/p5LongestPalindromicSubstring.java
@@ -0,0 +1,42 @@
+package TopInterviewQuestions;
+
+import java.util.Scanner;
+
+public class p5LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String palindrome = longestPalindrome(input);
+        System.out.println(palindrome);
+    }
+
+    private static int lo;
+    private static int maxLen;
+
+    public static String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        for (int i = 0; i < len-1; i++) {
+            extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+            extendPalindrome(s, i, i+1); //assume even length.
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    private static void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }}
