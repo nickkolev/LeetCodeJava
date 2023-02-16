1c46fde88cfd6222fcc7a323869f12e3d667a1c5
Nikola Kolev <nickkolev06@abv.bg>
2023-02-16 23:43:26 +0200

    dynamic programming approach

--- src/TopInterviewQuestions/p70ClimbingStairs.java
+++ src/TopInterviewQuestions/p70ClimbingStairs.java
@@ -26,4 +26,21 @@ public class p70ClimbingStairs {
             return recursion(x - 1) + recursion(x - 2);
         }
     }
+
+    //DP
+    public int dynamicApproach(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
 }
