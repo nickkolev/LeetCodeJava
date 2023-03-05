4d2d9fbdce93cbdb111d1947d408c6abf27feebb
Nikola Kolev <nickkolev06@abv.bg>
2023-03-05 23:17:37 +0200

    asd

--- src/TopInterviewQuestions/p121BestTimeToBuyAndSellStock.java
+++ src/TopInterviewQuestions/p121BestTimeToBuyAndSellStock.java
@@ -5,12 +5,14 @@ public class p121BestTimeToBuyAndSellStock {
 
     }
 
+
     public static int maxProfit(int[] prices) {
         int currentMinPrice = prices.length > 0 ? prices[0] : 0;
         int profit = 0;
         for (int i = 0; i < prices.length; i++) {
             currentMinPrice = Math.min(currentMinPrice, prices[i]);
             profit = Math.max(profit, prices[i] - currentMinPrice);
+
         }
         return profit;
     }
