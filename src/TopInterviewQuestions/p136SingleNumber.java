6a7974581b6c973b26adbeb09ce4a39279909e37
Nikola Kolev <nickkolev06@abv.bg>
2023-03-09 19:50:17 +0200

    Single number

---
+++ src/TopInterviewQuestions/p136SingleNumber.java
@@ -0,0 +1,19 @@
+package TopInterviewQuestions;
+
+public class p136SingleNumber {
+    public static void main(String[] args) {
+
+        System.out.println(singleNumber(new int[] {1, 1, 2, 3, 3, 4, 5, 6, 4, 5, 6}));
+    }
+
+    public static int singleNumber(int[] nums) {
+
+        int result = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+
+        return result;
+    }
+}
