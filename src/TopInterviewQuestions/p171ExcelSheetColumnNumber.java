31c9e529354a3bf4e215c85a0db448c2902be8ac
Nikola Kolev <nickkolev06@abv.bg>
2023-03-11 18:12:13 +0200

    faster solution

--- src/TopInterviewQuestions/p171ExcelSheetColumnNumber.java
+++ src/TopInterviewQuestions/p171ExcelSheetColumnNumber.java
@@ -17,4 +17,14 @@ public class p171ExcelSheetColumnNumber {
         return sum;
 
     }
+
+    /* faster solution
+    int result  = 0;
+        for (int i = 0; i < columnTitle.length(); i++){
+            result *= 26;
+            result += ((columnTitle.charAt(i) - 'A') + 1);
+        }
+
+        return result;
+     */
 }
