ad6d475facc1775cad4ed698f724a24f340deb65
Nikola Kolev <nickkolev06@abv.bg>
2023-03-21 22:27:01 +0200

    242-ValidAnagram

---
+++ src/TopInterviewQuestions/p242ValidAnagram.java
@@ -0,0 +1,24 @@
+package TopInterviewQuestions;
+
+public class p242ValidAnagram {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+        for(int i:count){
+            if(i!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
