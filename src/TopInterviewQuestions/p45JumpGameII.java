dad0dbe54e35e62d27833c29a9b1bc429312a054
Nikola Kolev <nickkolev06@abv.bg>
2023-04-20 23:15:14 +0300

    different approach

--- src/TopInterviewQuestions/p45JumpGameII.java
+++ src/TopInterviewQuestions/p45JumpGameII.java
@@ -18,4 +18,44 @@ public class p45JumpGameII {
         }
         return sc;
     }
+
+    public static int jump2(int[] nums) {
+        final int size = nums.length;
+
+        // destination is last index
+        int destination = size-1;
+
+        int curCoverage = 0, lastJumpIdx = 0;
+
+        // counter of jump
+        int timesOfJump = 0;
+
+        // Quick response if start index == destination index == 0
+        if( size == 1 ){
+            return 0;
+        }
+
+
+        // Greedy stragegy: extend coverage as long as possible with lazp jump
+        for( int i = 0 ; i < size ; i++){
+
+            // extend coverage
+            curCoverage = Math.max(curCoverage, i + nums[i] );
+
+            // forced to jump (by lazy jump) to extend coverage
+            if( i == lastJumpIdx ){
+
+                lastJumpIdx = curCoverage;
+
+                timesOfJump++;
+
+                // check if we reached destination already
+                if( curCoverage >= destination){
+                    return timesOfJump;
+                }
+            }
+        }
+
+        return timesOfJump;
+    }
 }
