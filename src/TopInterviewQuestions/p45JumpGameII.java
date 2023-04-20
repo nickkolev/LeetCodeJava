f73532c942352a92081a69348e96b0e81a6ec677
Nikola Kolev <nickkolev06@abv.bg>
2023-04-20 23:14:12 +0300

    solved

---
+++ src/TopInterviewQuestions/p45JumpGameII.java
@@ -0,0 +1,21 @@
+package TopInterviewQuestions;
+
+public class p45JumpGameII {
+    public static void main(String[] args) {
+
+    }
+
+    public static int jump(int[] nums) {
+        int sc = 0;
+        int e = 0;
+        int max = 0;
+        for(int i=0; i<nums.length-1; i++) {
+            max = Math.max(max, i+nums[i]);
+            if( i == e ) {
+                sc++;
+                e = max;
+            }
+        }
+        return sc;
+    }
+}
